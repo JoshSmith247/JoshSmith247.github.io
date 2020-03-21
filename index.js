5e706337f4ec1022c757bbbf1afa3a155f6581ee
JoshSmith247 <54862229+JoshSmith247@users.noreply.github.com>
2020-03-21 10:43:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //alert("TEST");
+function read_text_file (file) {	
+	if(file !=null) {
+		var all_string = "";
+
+		file.open("r");
+		while(!file.eof) { 
+			all_string += (file.readln() + "\n") ;
+		};
+		file.close();
+	};	
+	return all_string;
+};
+
+read_text_file("description.txt);
